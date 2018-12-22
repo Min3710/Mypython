087dcb822d3f3794c3e306ae0fa793b5e34aa958
Min3710 <45708257+Min3710@users.noreply.github.com>
2018-12-22 16:57:50 +0900

    chapter05
    chapter05

---
+++ chapter5/ex02.py
@@ -0,0 +1,6 @@
+age=20
+if age>30 and age<=50:
+    print("성년입니다다")
+else:
+    print("성년이 아닙니디.")
+    
---
+++ chapter5/ex03.py
@@ -0,0 +1,11 @@
+import random
+
+num1=random.randint(1,100)
+num2=random.rnadint(1,100)
+
+com_answer=num1-num2
+
+if com_answer==answer:
+    print("정답입니다.")
+else:
+    print("오답입니다.")
